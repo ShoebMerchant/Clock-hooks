324c5f93bd41693c76afe7bd558f6106b1ee784c
Shoeb Merchant <shoebmerchant12@gmail.com>
2021-10-24 01:07:02 +0530

    Clock Working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+
+let time = new Date().toLocaleTimeString();
 
 function App() {
+  const [timeToDisplay, setTime] = useState(time);
+
+  function updateTime() {
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  }
+  setInterval(updateTime, 1000);
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{timeToDisplay}</h1>
+      <button onClick={updateTime}>Get Time</button>
     </div>
   );
 }
